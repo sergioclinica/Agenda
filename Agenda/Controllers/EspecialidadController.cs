bf4280ad331f51708750d0a902d0242dad03d031
sergio <sergiogrdztrabajo@gmail.com>
2023-07-21 11:58:39 -0600

    Se complementa el CRUD de la Entidad Especialidad, se modifica la tabla Especialidad en AgendaContext

--- Agenda/Controllers/EspecialidadController.cs
+++ Agenda/Controllers/EspecialidadController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Agenda.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace Agenda.Controllers
 {
@@ -15,9 +17,89 @@ namespace Agenda.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View(_context.Especialidad.ToList());
+            return View(await _context.Especialidad.ToListAsync());
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();  
+            }
+
+            var especialidad = await _context.Especialidad.FindAsync(id);
+
+            if(especialidad == null) { 
+                return NotFound();
+            }
+
+            return View(especialidad);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("IdEspecialidad, Descripcion")] Especialidad especialidad)
+        {
+            if(id != especialidad.IdEspecialidad)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(especialidad);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(especialidad);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var especialidad = await _context.Especialidad.FirstOrDefaultAsync(e => e.IdEspecialidad == id);
+
+            if(especialidad == null)
+            {
+                return NotFound();
+            }
+
+            return View(especialidad);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var especialidad = await _context.Especialidad.FindAsync(id);   
+            _context.Especialidad.Remove(especialidad);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("IdEspecialidad, Descripcion")] Especialidad especialidad)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(especialidad);
+                //_context.Especialidad.Add(especialidad);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(especialidad);
+        }
+
     }
 }
--- Agenda/Models/AgendaContext.cs
+++ Agenda/Models/AgendaContext.cs
@@ -17,5 +17,17 @@ namespace Agenda.Models
         }
 
         public DbSet<Especialidad> Especialidad { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Especialidad>(entidad => {
+                entidad.ToTable("Especialidad"); // Es el nombre ara la tabla, pero se puede modificar
+                entidad.HasKey(e => e.IdEspecialidad);  // se especifica como clave primaria
+                entidad.Property(e => e.Descripcion)  // se agrega el nuevo campo Descripcion
+                    .IsRequired()   // indica que no se aceptan NULL
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+        }
     }
 }
