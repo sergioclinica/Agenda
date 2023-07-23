665616a24db04cbda546e85d51870df36cd0417a
sergio <sergiogrdztrabajo@gmail.com>
2023-07-22 19:04:29 -0600

    En PecienteController se agrega: El método Details. Los métodos Create, el GET y el POST y ValidateAntiForgeryToken

--- Agenda/Models/PacienteController.cs
+++ Agenda/Models/PacienteController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Agenda.Models
@@ -15,6 +16,41 @@ namespace Agenda.Models
         public async Task<IActionResult> Index()
         {
             return View(await _context.Paciente.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id) {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var paciente = await _context.Paciente.FirstOrDefaultAsync(p => p.IdPaciente == id);
+
+            if(paciente == null)
+            {
+                return NotFound();
+            }
+
+            return View(paciente);
+        }
+
+        public IActionResult create()
+        {
+            return View();
+        }
+
+        [HttpPatch]
+        [ValidateAntiForgeryToken] // Gatantiza que el método se ejecute desde el formulario de la palicación
+        public async Task<IActionResult> Create([Bind("IdPaciente, Nombre, Apellido, Direccion, Telefono, Email")] Paciente paciente)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Paciente.Add(paciente);
+                await _context.SaveChangesAsync();  
+                return RedirectToAction(nameof(Index)); 
+            }
+
+            return View(paciente);
         }
     }
 }
