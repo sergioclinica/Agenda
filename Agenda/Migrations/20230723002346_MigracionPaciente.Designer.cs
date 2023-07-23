9849208e20f8608786f523629b7a08af4b731daf
sergio <sergiogrdztrabajo@gmail.com>
2023-07-22 18:27:06 -0600

    Se crea la migración para Paciente

--- Agenda/Migrations/20230723002346_MigracionPaciente.Designer.cs
+++ Agenda/Migrations/20230723002346_MigracionPaciente.Designer.cs
@@ -3,14 +3,16 @@ using Agenda.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Agenda.Migrations
 {
     [DbContext(typeof(AgendaContext))]
-    partial class AgendaContextModelSnapshot : ModelSnapshot
+    [Migration("20230723002346_MigracionPaciente")]
+    partial class MigracionPaciente
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
