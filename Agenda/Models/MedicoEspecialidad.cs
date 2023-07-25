aa05bd7b049837a4f32b68f6c530a61d96a69fc8
sergio <sergiogrdztrabajo@gmail.com>
2023-07-25 07:58:08 -0600

    67. Se crea el modelo de relación MedicoEspecialidad

---
+++ Agenda/Models/MedicoEspecialidad.cs
@@ -0,0 +1,10 @@
+﻿namespace Agenda.Models
+{
+    public class MedicoEspecialidad
+    {
+        public int IdMedico { get; set; }
+        public int IdEspecialidad {get; set;}
+        public Medico Medico { get; set;}
+        public Especialidad Especialidad { get; set;}
+    }
+}
