9ff076861ff0c221649e610f0652101cf3c901e2
sergio <sergiogrdztrabajo@gmail.com>
2023-07-26 12:39:36 -0600

    79. Método Detalle (Se modifica el método MedicoController.Details y la vista Details para agregar la Especialidad)

--- Agenda/Controllers/MedicoController.cs
+++ Agenda/Controllers/MedicoController.cs
@@ -32,8 +32,11 @@ namespace Agenda.Controllers
                 return NotFound();
             }
 
-            var medico = await _context.Medico
-                .FirstOrDefaultAsync(m => m.IdMedico == id);
+            var medico = await _context.Medico.
+                            Where(m => m.IdMedico == id)
+                            .Include(me => me.MedicoEspecialidad)
+                            .ThenInclude(e => e.Especialidad)
+                            .FirstOrDefaultAsync();
             if (medico == null)
             {
                 return NotFound();
