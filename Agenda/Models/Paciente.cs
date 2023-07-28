6f71b56b2a60888176ed9179f04784647a6cece8
sergio <sergiogrdztrabajo@gmail.com>
2023-07-28 12:31:31 -0600

    106. EJERCICIO: Vista Paciente Se especifican las DataAnotations del modelo Paciente

--- Agenda/Models/Paciente.cs
+++ Agenda/Models/Paciente.cs
@@ -1,15 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Agenda.Models
 {
     public class Paciente
     {
         [Key]
-        public int IdPaciente { get; set; } 
+        public int IdPaciente { get; set; }
+        
+        [Required(ErrorMessage = "El dato  es obligatorio")]
+        [StringLength(50, ErrorMessage = "La longitud máximas es de 50 caracterres")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El dato  es obligatorio")]
+        [StringLength(50, ErrorMessage = "La longitud máximas es de 50 caracterres")]
         public  string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El dato  es obligatorio")]
+        [DisplayName("Dirección")]
+        [StringLength(50, ErrorMessage = "La longitud máximas es de 250 caracterres")]
         public string Direccion { get; set; }
+
+        [Required(ErrorMessage = "El dato  es obligatorio")]
+        [DisplayName("Teléfno")]
         public string Telefono { get; set;}
+
+        [Required(ErrorMessage = "El dato  es obligatorio")]
+        [EmailAddress]
+        [StringLength(50, ErrorMessage = "La longitud máximas es de 100 caracterres")]
         public string Email { get; set; }
     }
 }
