da42cd52a41b9452f3f782c698c2114e947af612
sergio <sergiogrdztrabajo@gmail.com>
2023-07-28 14:30:48 -0600

    110. Controlador Turno: Método Index Solo se crea el métod Index y el constructor de TurnoController

---
+++ Agenda/Controllers/TurnoController.cs
@@ -0,0 +1,31 @@
+﻿using Agenda.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace Agenda.Controllers
+{
+    public class TurnoController : Controller
+    {
+        private readonly AgendaContext _context;
+        private IConfiguration _configuration;
+
+        public TurnoController(AgendaContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["IdMedico"] = new SelectList((from medico in _context.Medico.ToList() 
+                                                   select new { IdMedico = medico.IdMedico, NombreCompleto = medico.Nombre + " " + 
+                                                   medico.Apellido }), "IdMedico", "NombreCompleto");
+            ViewData["IdPeciente"] = new SelectList((from paciente in _context.Paciente.ToList()
+                                                   select new { IdPaciente = paciente.IdPaciente, NombreCompleto = paciente.Nombre + " " + 
+                                                   paciente.Apellido }), "IdPaciente", "NombreCompleto");
+            return View();
+        }
+    }
+}
